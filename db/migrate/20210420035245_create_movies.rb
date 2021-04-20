f177bab27f7d35c39fb4aeb39d952f682f51653c
Blaire <grant.blaire@gmail.com>
2021-04-19 22:55:25 -0500

    Generated models for User and Movie. Completed migration tables for Users and Movies.

---
+++ db/migrate/20210420035245_create_movies.rb
@@ -0,0 +1,24 @@
+class CreateMovies < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movies do |t|
+      t.string :Title
+      t.string :Year
+      t.string :Rated
+      t.string :Released
+      t.string :Runtime
+      t.string :Genre
+      t.string :Director
+      t.text :Writer
+      t.text :Actors
+      t.text :Plot
+      t.text :Awards
+      t.string :Poster
+      t.string :Ratings
+      t.string :imdbID
+      t.string :BoxOffice
+      t.string :Production
+      t.boolean :Response
+      t.timestamps
+    end
+  end
+end
