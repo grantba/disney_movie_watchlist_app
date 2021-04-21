a62e022bb3e7954f5f77af2eab49d1799d2ab30e
Blaire <grant.blaire@gmail.com>
2021-04-21 11:12:57 -0500

    Added search_by_name and search_by_id actions to movies controller as well as strong params which will allow the search query.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,5 +7,21 @@ class MoviesController < ApplicationController
     def show
         @movie = Movie.find_by(id: params[:id])
     end
+
+    def search_by_name
+        @movie = Api.get_movie_by_name(params[:search])
+        redirect_to movie_path(@movie)
+    end
+
+    def search_by_id
+        @movie = Api.get_movie_by_id(params[:imdbID])
+        redirect_to movie_path(@movie)
+    end
+
+    private
+
+    def movie_params
+        params.require(:movie).permit(:search)
+    end
     
 end
