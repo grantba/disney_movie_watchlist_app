b70edfc019511b06e367e463779ae192eddeb7c4
Blaire <grant.blaire@gmail.com>
2021-04-21 11:15:29 -0500

    Added index and show actions to watchlists_controller.

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,3 +1,11 @@
 class WatchlistsController < ApplicationController
 
+    def index
+        @watchlists = Watchlist.all
+    end
+
+    def show
+        @watchlist = Watchlist.find_by(id: params[:id])
+    end
+    
 end
