438807ed5cd166425b4889b33606f4218529c043
Blaire <grant.blaire@gmail.com>
2021-04-21 11:14:05 -0500

    Added index and show actions to reviews_controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,11 @@
 class ReviewsController < ApplicationController
 
+    def index
+        @reviews = Review.all
+    end
+
+    def show
+        @review = Review.find_by(id: params[:id])
+    end
+    
 end
