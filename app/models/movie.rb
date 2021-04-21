6b6abb3ccf770575410df5c26da078fb39c9c079
Blaire <grant.blaire@gmail.com>
2021-04-20 23:07:06 -0500

    Added make_a_movie method and update_imdbID methods to Movie model. The make_a_movie method will create a new movie for each movie pulled from the WebScrapper model. This method is called from the parse method in the Webscrapper model. Once all movies from the array have been created in the make_a_movie method, it calls the update_imdbID method which will be used to create the proper imdbID to match the user's movie selection from the list to correct movie in the omdb Api.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,19 @@ class Movie < ApplicationRecord
     has_and_belongs_to_many :watchlists, through: :movie_watchlists
     has_many :users, through: :watchlists
     has_many :reviews
+
+    def self.make_a_movie(movies_array)
+        movies_array.each do |movie|
+          Movie.create(movie)
+        end
+        update_imdbID
+    end
+
+    def self.update_imdbID
+        self.all.each do |movie|
+            id = movie.imdbID.gsub("/title/", "").gsub("/?ref_=ttls_li_i", "")
+            movie.update(imdbID: id)
+        end
+    end
+
 end
