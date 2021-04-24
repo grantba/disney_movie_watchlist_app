e7705a85a97dfcafbe691f280eb14eb6b6b07afd
Blaire <grant.blaire@gmail.com>
2021-04-24 01:22:00 -0500

    Added logged_in?, current_user, and redirect_if_not_logged_in methods to application_controller in controllers. Set logged_in? and current_user methods as helper_methods to be used in views as well.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?, :current_user
+
+    def logged_in?
+        !!current_user
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+        redirect_to '/' if !logged_in?
+    end
+
 end
