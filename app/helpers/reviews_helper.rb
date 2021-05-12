de9c44ae9835299d46427ad5aaf72703c6b12faa
Blaire <grant.blaire@gmail.com>
2021-05-12 01:31:09 -0500

    Added review_description method to titleize review's description and updated reviews_by_user method to order reviews by movie title in reviewshelper module. Updated _review.html.erb and index.html.erb files to call new review_description method in reviewshelper module.

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,11 +2,16 @@ module ReviewsHelper
 
     def reviews_by_user
         reviews = Review.all.where(user_id: current_user.id)
+        reviews = reviews.sort_by { |r| r.movie.Title }
     end
 
     def remove_reviews_from_user
         reviews = reviews_by_user
         reviews.each { |r| r.destroy }
+    end
+
+    def review_description(review)
+        review.description.titleize
     end
 
 end
